c979750d3b54ff5c76a4b8739c39a55565cd4c1e
Parsa <alh.parsa@gmail.com>
2017-03-26 03:07:54 -0700

    Fixed the buttons on browse page

--- HACKtion/app/src/main/java/com/example/jryzkns/hacktion/browsePage.java
+++ HACKtion/app/src/main/java/com/example/jryzkns/hacktion/browsePage.java
@@ -1,13 +1,20 @@
 package com.example.jryzkns.hacktion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class browsePage extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_page);
+    }
+    public void sex(View MainActivity){
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
