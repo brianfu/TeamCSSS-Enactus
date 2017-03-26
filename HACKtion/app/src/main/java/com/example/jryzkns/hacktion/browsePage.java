e6477eabb28f0e062564b57b9654e7e6ca6f95b0
plasmalaser <Fu.Brian@outlook.com>
2017-03-26 10:54:05 -0700

    commit b4 testing randoms

--- HACKtion/app/src/main/java/com/example/jryzkns/hacktion/browsePage.java
+++ HACKtion/app/src/main/java/com/example/jryzkns/hacktion/browsePage.java
@@ -1,8 +1,11 @@
 package com.example.jryzkns.hacktion;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class browsePage extends AppCompatActivity {
@@ -12,9 +15,19 @@ public class browsePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                sex(view);
+            }
+        });
     }
-    public void sex(View sampleReport){
-        Intent intent=new Intent(this, sampleReport.class);
+    public void sex(View report){
+        Intent intent=new Intent(this, report.class);
         startActivity(intent);
     }
 }
