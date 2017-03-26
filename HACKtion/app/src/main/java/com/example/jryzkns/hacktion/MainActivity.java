645ddfeaec817755342e99797824a79bf451b234
jryzkns <xza194@sfu.ca>
2017-03-25 18:01:02 -0700

    commit testing code changes, check for random comment in main activity

--- HACKtion/app/src/main/java/com/example/jryzkns/hacktion/MainActivity.java
+++ HACKtion/app/src/main/java/com/example/jryzkns/hacktion/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //random commit test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
