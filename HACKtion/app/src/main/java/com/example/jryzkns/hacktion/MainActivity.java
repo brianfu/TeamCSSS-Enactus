354f74a24571925057a7915396874332a1094ad6
brianfu <Fu.Brian@outlook.com>
2017-03-25 18:53:34 -0700

    lag sorry

--- HACKtion/app/src/main/java/com/example/jryzkns/hacktion/MainActivity.java
+++ HACKtion/app/src/main/java/com/example/jryzkns/hacktion/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     //random commit test
+    //random commit 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
